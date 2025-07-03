b7c49d8b5792ad8cff855e3c6190b9fc23a30bfa
Chahnez Zurawicz <cvkroyal@hotmail.com>
2025-07-03 20:10:41 +0200

    adding users and seeds

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
+gem "devise"
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
@@ -56,4 +57,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(published_at: :desc)
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comment = comment.new
   end
 
   def new
@@ -19,4 +22,11 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :location, :image_url, :published_at)
+  end
+
 end
